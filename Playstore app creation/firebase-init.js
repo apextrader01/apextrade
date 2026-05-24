5ec0ccef8755d1fb9409663bb8652a632805d6bf
apextrader01 <appwebsitetester@gmail.com>
2026-05-24 16:49:01 +0530

    Update firebase-init.js

--- Playstore app creation/firebase-init.js
+++ Playstore app creation/firebase-init.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
-// TODO: Replace this with your actual Firebase Config object
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AKfycbwKWfkH0yGcH-wf0IE7NByB_ZqqbZPaalaQMuNuGVClzYeBpIeCvrbYTGjq6s5amugh", 
+  authDomain: "apextrade-live.firebaseapp.com",
+  projectId: "apextrade-live",
+  storageBucket: "apextrade-live.appspot.com",
+  messagingSenderId: "192611180862",
+  appId: "1:192611180862:web:f6f306d3886cea95ec0c6f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Auth & Firestore and export them so app.js can use them
 export const auth = getAuth(app);
 export const db = getFirestore(app);
